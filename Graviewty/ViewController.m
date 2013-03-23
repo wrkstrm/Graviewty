5a68d5cd676ee1d4b534b81b363b6ec01c4ad914
rismay <cmonterroza@gmail.com>
2013-03-23 14:45:13 -0400

    Initial Commit: Includes, the official wrkstrm colors, simple gravity button sub-class and a sample view - the wrkstrm logo.

--- Graviewty/ViewController.m
+++ Graviewty/ViewController.m
@@ -7,23 +7,31 @@
 //
 
 #import "ViewController.h"
+#import "WSVectorLogoView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) WSVectorLogoView *smallVectorLogo;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (id) initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    if (self) {}
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.smallVectorLogo = [[WSVectorLogoView alloc] initWithFrame: CGRectMake(0, 0, 280, 170)];
+    self.smallVectorLogo.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    self.smallVectorLogo.enabled = YES;
+    [self.view addSubview: self.smallVectorLogo];
+    [self.smallVectorLogo pulseView: YES];
 }
+
+- (void)didReceiveMemoryWarning {[super didReceiveMemoryWarning];}
 
 @end
