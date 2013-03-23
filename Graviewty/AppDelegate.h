7312e5da12f6d1e9746289a4602eed63ac851b27
rismay <cmonterroza@gmail.com>
2013-03-23 13:35:19 -0400

    Initial Commit

---
+++ Graviewty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Graviewty
+//
+//  Created by Cristian A Monterroza on 3/23/13.
+//  Copyright (c) 2013 wrkstrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Graviewty/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Graviewty
+//
+//  Created by Cristian A Monterroza on 3/23/13.
+//  Copyright (c) 2013 wrkstrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
